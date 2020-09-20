54780d8b36da82ca5b79aa1e8aa2ffb463d56b5b
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-09-20 15:40:20 +0500

    Add project files.

---
+++ Шахматные фигуры/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Шахматные_фигуры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
